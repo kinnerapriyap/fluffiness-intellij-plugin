f4256b58e75ab206733a4c5311e8bdef75b76acd
GitHub Action <action@github.com>
2021-03-27 06:54:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kinnerapriyap/fluffinessintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kinnerapriyap.fluffinessintellijplugin.services
+
+import com.github.kinnerapriyap.fluffinessintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kinnerapriyap/fluffinessintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kinnerapriyap/fluffinessintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kinnerapriyap.fluffinessintellijplugin.services
 
+import com.github.kinnerapriyap.fluffinessintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
